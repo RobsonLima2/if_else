51142d2924dda4484b93cac0ac58ea188c03f7aa
RobsonLima2 <robson.ramos.lima@hotmail.com>
2022-06-13 21:17:34 -0300

    if and else

---
+++ script.js
@@ -0,0 +1,58 @@
+// /*******************************************************************************************************************************************/
+// //1 - Crie uma condição que exiba uma mensagem no console se a idade for maior que 18-->ok
+// let idade = 20
+
+// if (idade > 18) {
+//     console.log("Sua idade é maior que 18")
+//     }else{
+//     console.log('Sua idade é menor ou igual a 18')
+// }
+
+
+
+// /*******************************************************************************************************************************************/
+// //2 - Crie uma condição que exiba uma mensagem no console se você for maior de idade E a condição humana seja true-->ok
+// // let idade = 19
+// // let maiorIdade = 18
+// // let humana  = true
+
+// // if (idade>maiorIdade && humana==true) {
+// //     console.log("Você é maior de idade e humano")
+    
+// // } else {
+// //     console.log("Você é menor de idade não é humano")
+// // }
+
+
+
+// /*******************************************************************************************************************************************/
+
+// //3 - Criar uma condição que exiba uma mensagem no console se você fizer aniversário em Janeiro OU Dezembro
+
+// let aniversario = 12
+
+// if(aniversario==1 || aniversario==12){
+//     console.log('Seu aniversário é em Janeiro ou em Dezembro')
+// }else{
+//     console.log('Seu aniversário não é em Janeiro e nem em Dezembro')
+// }
+
+// /*******************************************************************************************************************************************/
+
+// //4 - Criar uma condição que exiba uma mensagem no console se seu nome começar com a letra R
+
+// const nome = 'Robson'
+
+// if(nome.startsWith('R')){
+//     console.log('Seu nome começa com R')
+// }else{
+//     console.log('Seu nome não começa com R')
+// }
+
+
+// /*******************************************************************************************************************************************/
+
+
+// //5 - Criar uma condição que exiba uma mensagem no console se seu sobrenome tenha mais de 6 letras OU seu nome começar com a letra E
+
+// /*******************************************************************************************************************************************/
